94f85f410028ba2979f752dd139a2f14e48e8883
Jeishy <k1519236@kingston.ac.uk>
2019-03-29 05:25:41 +0000

    Update

---
+++ Unity Shaders Showcase/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private ToggleScenes _toggleScenes;
+
+    private void Start()
+    {
+        _toggleScenes = GetComponent<ToggleScenes>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _toggleScenes.LoadLastScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _toggleScenes.LoadNextScene();
+        }
+    }
+}
--- Unity Shaders Showcase/Assets/Scripts/ToggleScenes.cs
+++ Unity Shaders Showcase/Assets/Scripts/ToggleScenes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ToggleScenes : MonoBehaviour
-{
+{  
     private ShaderDescriptionLoader descriptionLoader;
     private SceneCountTextUpdate sceneCountUpdate;
 
